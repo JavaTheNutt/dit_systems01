10b8b56d5d9d2b3d6ba9ad9f8ca5e60b3db944f1
Joe Wemyss <support@javathenutt.com>
2018-01-25 20:43:39 +0000

    updated migration path

--- migrations/20180125100057_create_facilities_table.js
+++ migrations/20180125100057_create_facilities_table.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('facility', t => {
     t.increments('f_id').primary();
-    t.string('f_name').notNullable();
+    t.string('f_name').notNullable().unique();
   })
 };
 
--- migrations/20180125105240_add_email_to_user.js
+++ migrations/20180125105240_add_email_to_user.js
@@ -6,7 +6,7 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.table('user', t => {
+   return knex.schema.table('user', t => {
     t.dropColumn('u_email');
   });
 };
